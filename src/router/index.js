3c3865175b00161eb5cca9a7384672c935240566
Diogo G <dlgreis88@gmail.com>
2022-09-11 17:59:44 +0200

    loveya

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Test from "@/views/Test";
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+
+  {
+    path: '/test',
+    name: 'test',
+    component: Test
   },
 ]
 
---
+++ src/views/Test.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div id="particles-container">
+    <Particles
+        id="tsparticles"
+        url="particles.json"
+        :particles-init="initializeParticles"/>
+  </div>
+
+</template>
+
+<script>
+import { loadFull } from "tsparticles";
+
+export default {
+  name: "Test",
+
+  methods: {
+    async initializeParticles(engine) {
+      await loadFull(engine)
+    }
+  }
+}
+</script>
